45b5d10947ff3b444966675552b486ce14c6c5f1
André Simiquelli <andresimiquelli@hotmail.com>
2021-08-26 09:10:14 -0300

    página localização

---
+++ pages/localizacao.php
@@ -0,0 +1,6 @@
+<h1>Localização</h1>
+<p>Estamos situados na charmosa cidade histórica de Tiradentes, a 192 km de BH pela rodovia 040.</p>
+<h2>Endereço</h2>
+<h3>Rua Alvarenga Peixoto, 33. Centro - Tiradentes MG</h3>
+<iframe style="margin: 20px 0 30px 0" width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=tiradentes&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+  
